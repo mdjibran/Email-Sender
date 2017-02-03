32e5cf97c6f38d058e2e0c51cfdc36e14bb82218
mdjibran <mohdjibran316@gmail.com>
2017-02-03 11:12:23 -0500

    Email Sender Initialised

---
+++ EmailSender/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace EmailSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this._tbsubject = new System.Windows.Forms.TextBox();
+            this._tbMessage = new System.Windows.Forms.RichTextBox();
+            this._tbfrom = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this._tbtoList = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this._listSentEmails = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this._tbto = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this._attchmentBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.size = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _tbsubject
+            // 
+            this._tbsubject.Location = new System.Drawing.Point(55, 40);
+            this._tbsubject.Name = "_tbsubject";
+            this._tbsubject.Size = new System.Drawing.Size(192, 20);
+            this._tbsubject.TabIndex = 0;
+            // 
+            // _tbMessage
+            // 
+            this._tbMessage.Location = new System.Drawing.Point(12, 154);
+            this._tbMessage.Name = "_tbMessage";
+            this._tbMessage.Size = new System.Drawing.Size(334, 254);
+            this._tbMessage.TabIndex = 1;
+            this._tbMessage.Text = "";
+            // 
+            // _tbfrom
+            // 
+            this._tbfrom.Location = new System.Drawing.Point(55, 12);
+            this._tbfrom.Name = "_tbfrom";
+            this._tbfrom.ReadOnly = true;
+            this._tbfrom.Size = new System.Drawing.Size(192, 20);
+            this._tbfrom.TabIndex = 2;
+            this._tbfrom.Text = "youremail@id.com";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(253, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Load List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "From :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Subject :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Message :";
+            // 
+            // _tbtoList
+            // 
+            this._tbtoList.FormattingEnabled = true;
+            this._tbtoList.Location = new System.Drawing.Point(6, 19);
+            this._tbtoList.Name = "_tbtoList";
+            this._tbtoList.Size = new System.Drawing.Size(340, 199);
+            this._tbtoList.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this._listSentEmails);
+            this.groupBox1.Controls.Add(this._tbtoList);
+            this.groupBox1.Location = new System.Drawing.Point(366, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(352, 408);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "To :";
+            // 
+            // _listSentEmails
+            // 
+            this._listSentEmails.FormattingEnabled = true;
+            this._listSentEmails.Location = new System.Drawing.Point(7, 229);
+            this._listSentEmails.Name = "_listSentEmails";
+            this._listSentEmails.Size = new System.Drawing.Size(339, 173);
+            this._listSentEmails.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 411);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Send Mail";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "To :";
+            // 
+            // _tbto
+            // 
+            this._tbto.Location = new System.Drawing.Point(55, 66);
+            this._tbto.Name = "_tbto";
+            this._tbto.Size = new System.Drawing.Size(192, 20);
+            this._tbto.TabIndex = 12;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 436);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(730, 22);
+            this.statusStrip1.TabIndex = 16;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(605, 442);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 13);
+            this.progressBar1.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(549, 441);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Progress :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 95);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "File";
+            // 
+            // _attchmentBox
+            // 
+            this._attchmentBox.Location = new System.Drawing.Point(55, 92);
+            this._attchmentBox.Name = "_attchmentBox";
+            this._attchmentBox.Size = new System.Drawing.Size(192, 20);
+            this._attchmentBox.TabIndex = 20;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(253, 90);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Attach";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // size
+            // 
+            this.size.AutoSize = true;
+            this.size.Location = new System.Drawing.Point(239, 138);
+            this.size.Name = "size";
+            this.size.Size = new System.Drawing.Size(0, 13);
+            this.size.TabIndex = 23;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(253, 8);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 17);
+            this.radioButton1.TabIndex = 24;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Single";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(253, 31);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 17);
+            this.radioButton2.TabIndex = 25;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Multiple";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 458);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.size);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this._attchmentBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this._tbto);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._tbfrom);
+            this.Controls.Add(this._tbMessage);
+            this.Controls.Add(this._tbsubject);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "EasyNotes Emailer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox _tbsubject;
+        private System.Windows.Forms.RichTextBox _tbMessage;
+        private System.Windows.Forms.TextBox _tbfrom;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox _tbtoList;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox _tbto;
+        private System.Windows.Forms.ListBox _listSentEmails;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox _attchmentBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label size;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ EmailSender/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using System.IO;
+
+
+namespace EmailSender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] fileList = new string[20];
+        float attachSize=0, totAttach=0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_tbto.Text != "" || _tbtoList.Items.Count > 0)
+                sendMail();
+            else
+                MessageBox.Show("Please enter a receivers email address or list of receivers to send mail","Email Sender");
+        }
+
+
+        private void sendMail()
+        {
+            if (radioButton1.Checked == true)
+                sendToSingle();
+            if (radioButton2.Checked == true)
+                SendToMany();
+        }
+
+        private void SendToMany()
+        {
+            progressBar1.Value = 0;
+            try
+            {
+                MailMessage msg = new MailMessage();
+                msg.From = new MailAddress(_tbfrom.Text, "EasyNotes.Co.In");
+                //              msg.To.Add(new MailAddress(_tbto.Text));
+                try
+                {
+                    msg.Attachments.Add(new Attachment(_attchmentBox.Text));
+                }
+                catch { }
+                msg.Subject = _tbsubject.Text;
+                msg.Body = _tbMessage.Text;
+                msg.IsBodyHtml = false;
+
+
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Credentials = new NetworkCredential(_tbfrom.Text, "9960065517");
+                smtp.EnableSsl = true;
+
+                int listLength, seriel = 1;
+                decimal barValue = 0;
+                listLength = _tbtoList.Items.Count;
+                barValue = (100 / listLength);
+
+
+                progressBar1.Value = 0;
+                smtp.Send(msg);
+
+                MessageBox.Show("All emails have been sent", "Easynotes Emailer");
+            }
+
+            catch
+            {
+                MessageBox.Show("Some Unknown Error occured, Please try after some time!!!", "Email Sender");
+                return;
+            }          
+        }
+
+        private void sendToSingle()
+        {
+            progressBar1.Value = 0;
+            try
+            {
+                MailMessage msg = new MailMessage();
+                msg.From = new MailAddress("studentsitehelp@gmail.com", "EasyNotes.Co.In");
+                msg.To.Add(new MailAddress(_tbto.Text));
+                try
+                {
+                    msg.Attachments.Add(new Attachment(_attchmentBox.Text));
+                }
+                catch { }
+                msg.Subject = _tbsubject.Text;
+                msg.Body = _tbMessage.Text;
+                msg.IsBodyHtml = false;
+
+
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Credentials = new NetworkCredential("studentsitehelp@gmail.com", "9960065517");
+                smtp.EnableSsl = true;
+
+                smtp.Send(msg);
+
+                MessageBox.Show("Email has been sent", "Easynotes Emailer");
+            }
+
+            catch
+            {
+                MessageBox.Show("Some Unknown Error occured, Please try after some time!!!", "Email Sender");
+                return;
+            }          
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _tbtoList.Items.Clear();
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {               
+
+                StreamReader sr = new StreamReader(ofd.FileName);
+                string text = File.ReadAllText(ofd.FileName).ToString();
+                string pureText = text.Replace("\r\n", "");
+
+                string[] spltredText = pureText.Split(',');
+                foreach (string s in spltredText)
+                {                                         
+                  _tbtoList.Items.Add(s);
+                  button2.Enabled = true;
+                }
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            button1.Enabled = false;
+            _tbto.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            float div;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string path = ofd.FileName.ToString();
+                FileInfo finfo = new FileInfo(path);
+                attachSize = Convert.ToInt32(finfo.Length);
+
+                div = attachSize / 1048576;
+                totAttach += div;
+                size.Text = div.ToString() + " MB";
+                _attchmentBox.Text = path;
+                //listBox1.Items.Add(path);
+                //int i = 0;
+                //foreach (string file in listBox1.Items)
+                //{
+                //    fileList[i] = file;
+                //    i++;
+                //}
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            _tbto.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            _tbto.Text = "";
+            _tbto.Enabled = false;
+        }        
+    }
+}
